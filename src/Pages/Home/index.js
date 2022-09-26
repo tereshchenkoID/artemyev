ca6a04096fb656b07d7d5d1e1e0f1ae47957c02d
i-tereshchenko <i.tereschenko@weplay.tv>
2022-09-26 21:39:42 +0300

    - random section

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import SectionPortfolio from "../../Sections/SectionPortfolio/index";
 import SectionResume from "../../Sections/SectionResume";
+import SectionRandom from "../../Sections/SectionRandom";
 
 const Home = () => {
   return (
       <>
-         <SectionPortfolio />
-         <SectionResume />
+          <SectionRandom />
+          <SectionPortfolio />
+          <SectionResume />
       </>
   )
 }
---
+++ src/Sections/SectionRandom/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+import TextSection from "../../Components/TextSection";
+import TextDecor from "../../Components/TextDecor";
+
+import styles from './index.module.scss'
+
+const SectionRandom = () => {
+  return (
+      <section id={'section-1'}>
+          <TextSection text={'Hello my friend'}/>
+          <div className={styles.top}>
+              <span className={styles.decor}>
+                  <svg width="86" height="22" viewBox="0 0 86 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" clipRule="evenodd" d="M18.2566 10.5245C13.9863 10.5244 10.5245 7.06257 10.5245 2.79224C10.5245 7.06263 7.06269 10.5245 2.7923 10.5245C7.06269 10.5245 10.5245 13.9863 10.5245 18.2567C10.5245 13.9864 13.9863 10.5246 18.2566 10.5245Z" fill="#23262D"/>
+                      <circle cx="10.6104" cy="10.6104" r="10.1104" stroke="#23262D"/>
+                      <path fillRule="evenodd" clipRule="evenodd" d="M50.6462 10.5245C46.3759 10.5244 42.9141 7.06258 42.9141 2.79224C42.9141 7.06263 39.4523 10.5245 35.1819 10.5245C39.4523 10.5245 42.9141 13.9863 42.9141 18.2567C42.9141 13.9864 46.3759 10.5245 50.6462 10.5245Z" fill="#23262D"/>
+                      <circle cx="43" cy="10.6104" r="10.1104" stroke="#23262D"/>
+                      <path fillRule="evenodd" clipRule="evenodd" d="M83.0359 10.5245C78.7655 10.5244 75.3038 7.0626 75.3038 2.79224C75.3038 7.06263 71.8419 10.5245 67.5715 10.5245C71.8419 10.5245 75.3038 13.9863 75.3038 18.2567C75.3038 13.9863 78.7655 10.5245 83.0359 10.5245Z" fill="#23262D"/>
+                      <circle cx="75.3897" cy="10.6104" r="10.1104" stroke="#23262D"/>
+                  </svg>
+              </span>
+              <h6>pleasant viewing</h6>
+          </div>
+          <div className={styles.bottom}>
+              <span className={styles.back}>
+                  <svg width="231" height="212" viewBox="0 0 231 212" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <ellipse cx="115.5" cy="106.215" rx="114.5" ry="104.597" stroke="#24262D"/>
+                      <path d="M114.797 1.61792C19.9615 1.61792 18.5668 210.812 114.797 210.812" stroke="#24262D"/>
+                      <path d="M114.798 1.61792C209.633 1.61792 211.027 210.812 114.798 210.812" stroke="#24262D"/>
+                      <path d="M230 107.293C230 178.655 2.40491 179.705 2.40491 107.293" stroke="#24262D"/>
+                      <path d="M2.40526 109.45C2.40526 38.0875 230 37.038 230 109.45" stroke="#24262D"/>
+                      <path d="M114.553 0.539795V211.89" stroke="#24262D"/>
+                      <path d="M1.70238 108.372C4.51219 108.372 155.071 108.372 230 108.372" stroke="#24262D"/>
+                      <path fillRule="evenodd" clipRule="evenodd" d="M53.9919 110.67C55.2429 110.576 56.505 110.528 57.7768 110.528C89.1338 110.528 114.554 139.495 114.554 175.227C114.554 140.944 137.953 112.889 167.545 110.67V110.387C137.953 108.167 114.554 80.1121 114.554 45.8291C114.554 81.5614 89.1338 110.528 57.7768 110.528C56.505 110.528 55.2429 110.481 53.9919 110.387L53.9919 110.67Z" fill="#F26C6C"/>
+                  </svg>
+              </span>
+              <div className={styles.wrapper}>
+                  <TextDecor text={'no program was affected'}/>
+                  <span className={styles.decor}>
+                      <svg width="58" height="53" viewBox="0 0 58 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <ellipse cx="29" cy="26.842" rx="28" ry="25.5783" stroke="#24262D"/>
+                          <path d="M28.8281 1.26367C5.63699 1.26367 5.29593 52.4203 28.8281 52.4203" stroke="#24262D"/>
+                          <path d="M28.8284 1.26367C52.0195 1.26367 52.3606 52.4203 28.8284 52.4203" stroke="#24262D"/>
+                          <path d="M57 27.1057C57 44.5568 1.34356 44.8134 1.34356 27.1057" stroke="#24262D"/>
+                          <path d="M1.34351 27.6331C1.34351 10.182 56.9999 9.92537 56.9999 27.6331" stroke="#24262D"/>
+                          <path d="M28.7686 1V52.684" stroke="#24262D"/>
+                          <path d="M1.17163 27.3694C1.85875 27.3694 38.6767 27.3694 56.9999 27.3694" stroke="#24262D"/>
+                          <path fillRule="evenodd" clipRule="evenodd" d="M13.9587 27.9314C14.2647 27.9084 14.5733 27.8968 14.8843 27.8968C22.5524 27.8968 28.7686 34.9804 28.7686 43.7184C28.7686 35.3348 34.4908 28.4741 41.7273 27.9314V27.8622C34.4908 27.3194 28.7686 20.4588 28.7686 12.0751C28.7686 20.8132 22.5524 27.8968 14.8843 27.8968C14.5733 27.8968 14.2647 27.8851 13.9587 27.8622V27.9314Z" fill="#F26C6C"/>
+                          <ellipse cx="28.992" cy="27.7477" rx="14.4814" ry="14.3337" transform="rotate(90 28.992 27.7477)" fill="#D9D9D9" stroke="#24262D"/>
+                          <ellipse cx="25.0552" cy="25.5755" rx="3.25832" ry="1.49411" transform="rotate(90 25.0552 25.5755)" fill="#23262D"/>
+                          <ellipse cx="32.6009" cy="25.5755" rx="3.25832" ry="1.49411" transform="rotate(90 32.6009 25.5755)" fill="#23262D"/>
+                          <path d="M19.9409 28.8339C20.9546 37.9192 35.7257 39.0408 38.0427 28.8339" stroke="#23262D"/>
+                      </svg>
+                  </span>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut </p>
+              </div>
+          </div>
+          <p>2</p>
+      </section>
+  )
+}
+
+export default SectionRandom;
